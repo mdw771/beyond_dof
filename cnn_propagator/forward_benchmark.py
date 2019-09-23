33aa6335a7ec02cddeba21d688722bd43f74ffbe
mdw771 <mingdu2015@u.northwestern.edu>
2019-09-23 13:29:39 -0500

    over ranks

--- cnn_propagator/forward_benchmark.py
+++ cnn_propagator/forward_benchmark.py
@@ -10,6 +10,7 @@ import warnings
 import math
 import numpy as nnp
 from tqdm import trange
+import sys
 import os
 from math import ceil, floor
 from propagation import multislice_propagate_cnn
@@ -33,42 +34,60 @@ lmbda_nm = 1.24 / (energy_ev / 1e3)
 psize_min_cm = 100e-7
 kernel_size_ls = 2 ** np.array([2, 3, 4, 5, 6]) + 1
 free_prop_cm = None
+######################################################################
 size_ls = np.array([256, 512, 1024, 2048, 4096])
+#####################################################################
 path_prefix = os.path.join(os.getcwd(), 'charcoal')
 n_repeats = 100
 
+# Start from where it stopped
+i_st = 0
+try:
+    f = open(os.path.join(path_prefix, 'size_{}'.format(size_ls[0]), 'timing.csv'), 'r')
+    a = f.readlines()[-1]
+    i_st = int(a[:a.find(',')])
+except:
+    pass
+
 # Create report
 if rank == 0:
-    f = open(os.path.join(path_prefix, 'report.csv'), 'w')
-    f.write('algorithm,object_size,kernel_size,safezone_width,avg_time,mse_with_fft\n')
+    f = open(os.path.join(path_prefix, 'report.csv'), 'a')
+#    f.write('algorithm,object_size,kernel_size,safezone_width,avg_time,mse_with_fft\n')
 
 # Do a FFT based propagation
-if rank == 0:
-    for this_size in size_ls:
-        grid_delta = dxchange.read_tiff(os.path.join(path_prefix, 'phantom', 'size_{}', 'grid_delta.tiff').format(this_size))
-        grid_beta = dxchange.read_tiff(os.path.join(path_prefix, 'phantom', 'size_{}', 'grid_beta.tiff').format(this_size))
-        grid_delta = np.reshape(grid_delta, [1, *grid_delta.shape])
-        grid_beta = np.reshape(grid_beta, [1, *grid_beta.shape])
-        probe_real = np.ones([*grid_delta.shape[1:3]])
-        probe_imag = np.zeros([*grid_delta.shape[1:3]])
-        size_factor = size_ls[-1] // this_size
-        psize_cm = psize_min_cm * size_factor
-        dt_ls = np.zeros(n_repeats)
+for this_size in size_ls:
+    # f_temp = open(os.path.join(path_prefix, 'size_{}'.format(this_size), 'timing.csv'), 'a')
+    grid_delta = dxchange.read_tiff(os.path.join(path_prefix, 'phantom', 'size_{}', 'grid_delta.tiff').format(this_size))
+    grid_beta = dxchange.read_tiff(os.path.join(path_prefix, 'phantom', 'size_{}', 'grid_beta.tiff').format(this_size))
+    grid_delta = np.reshape(grid_delta, [1, *grid_delta.shape])
+    grid_beta = np.reshape(grid_beta, [1, *grid_beta.shape])
+    probe_real = np.ones([*grid_delta.shape[1:3]])
+    probe_imag = np.zeros([*grid_delta.shape[1:3]])
+    size_factor = size_ls[-1] // this_size
+    psize_cm = psize_min_cm * size_factor
 
-        for i in trange(n_repeats):
-            t0 = time.time()
-            wavefield = multislice_propagate_batch_numpy(grid_delta, grid_beta, probe_real, probe_imag, energy_ev, [psize_cm] * 3, obj_batch_shape=grid_delta.shape)
-            dt = time.time() - t0
-            if i == 0:
-                dxchange.write_tiff(abs(wavefield), os.path.join(path_prefix, 'size_{}'.format(this_size), 'fft_output'), dtype='float32', overwrite=True)
-            dt_ls[i] = dt
-        dt_avg = np.mean(dt_ls)
-        print('FFT: For size {}, average dt = {} s.'.format(this_size, dt_avg))
+    for i in trange(i_st, n_repeats):
+        t0 = time.time()
+        wavefield = multislice_propagate_batch_numpy(grid_delta, grid_beta, probe_real, probe_imag, energy_ev, [psize_cm] * 3, obj_batch_shape=grid_delta.shape)
+        dt = time.time() - t0
+        if i == 0:
+            dxchange.write_tiff(abs(wavefield), os.path.join(path_prefix, 'size_{}'.format(this_size), 'fft_output'), dtype='float32', overwrite=True)
+        # f_temp.write('{},{}\n'.format(i, dt))
+        # f_temp.flush()
+        # os.fsync(f_temp.fileno())
+    dt_avg = 0
+    print('FFT (rank {}): For size {}, dt = {} s.'.format(rank, this_size, dt))
+    comm.Allreduce(dt, dt_avg)
+    dt_avg /= size
+
+    if rank == 0:
         f.write('fft,{},0,0,{},0\n'.format(this_size, dt_avg))
         f.flush()
         os.fsync(f.fileno())
 
 comm.Barrier()
+
+sys.exit()
 
 for this_size in size_ls:
 
