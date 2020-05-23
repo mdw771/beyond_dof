eca68d7c6a9b5fa9ab9780dd5af8545b07102422
mdw771 <cruiser0631@gmail.com>
2020-05-23 11:10:34 -0500

    Change GPU code

--- cnn_propagator/s_scaling_pfft_zp_gpu.py
+++ cnn_propagator/s_scaling_pfft_zp_gpu.py
@@ -15,6 +15,7 @@ import argparse
 import adorym
 import adorym.wrappers as w
 import adorym.global_settings as ag
+from adorym import print_flush
 
 ag.backend = 'pytorch'
 device_obj = w.get_device(0)
@@ -69,7 +70,7 @@ if rank == 0:
 if rank == 0:
     f = open(os.path.join(path_prefix, 'size_{}'.format(this_size), 'nd_{}'.format(n_nodes), 'report_pfft.csv'), 'w')
     if os.path.getsize(os.path.join(path_prefix, 'size_{}'.format(this_size), 'nd_{}'.format(n_nodes), 'report_pfft.csv')) == 0:
-        f.write('i_repeat,n_nodes,this_size,n_slices,safe_zone_width,n_blocks_y,n_blocks_x,block_size,n_ranks,dt_total,dt_read_div,dt_write,dt_fft_prop\n')
+        f.write('i_repeat,n_nodes,this_size,n_slices,safe_zone_width,n_blocks_y,n_blocks_x,block_size,n_ranks,dt_total,dt_read_div,dt_reshape_and_comm,dt_fft_prop\n')
 
 # Benchmark partial FFT propagation
 
@@ -124,10 +125,10 @@ for i in range(n_repeats):
     n_blocks = n_blocks_x * n_blocks_y
     block_size = ceil(max([original_grid_shape[0] / n_blocks_y, original_grid_shape[1] / n_blocks_x]))
     if rank == 0:
-        print('n_blocks_y: ', n_blocks_y)
-        print('n_blocks_x: ', n_blocks_x)
-        print('n_blocks: ', n_blocks)
-        print('block_size: ', block_size)
+        print_flush('n_blocks_y: ', n_blocks_y)
+        print_flush('n_blocks_x: ', n_blocks_x)
+        print_flush('n_blocks: ', n_blocks)
+        print_flush('block_size: ', block_size)
 
     this_pos_ind_ls = range(rank, n_blocks, n_ranks)
     block_delta_batch = np.zeros(
@@ -189,42 +190,46 @@ for i in range(n_repeats):
     if block_delta_batch.shape[0] > 0:
         comm.Barrier()
         # -----------------------------------------
-        block_delta_batch = w.create_variable(block_delta_batch, device=device_obj)
-        block_beta_batch = w.create_variable(block_beta_batch, device=device_obj)
+        t_reshape_0 = time.time()
+        block_delta_batch = w.create_variable(block_delta_batch, device=device_obj, requires_grad=False)
+        block_beta_batch = w.create_variable(block_beta_batch, device=device_obj, requires_grad=False)
         block = w.stack([block_delta_batch, block_beta_batch], -1)
-        block_probe_real_batch = w.create_variable(block_probe_real_batch, device=device_obj)
-        block_probe_imag_batch = w.create_variable(block_probe_imag_batch, device=device_obj)
+        block_probe_real_batch = w.create_variable(block_probe_real_batch, device=device_obj, requires_grad=False)
+        block_probe_imag_batch = w.create_variable(block_probe_imag_batch, device=device_obj, requires_grad=False)
+        dt_reshape = time.time() - t_reshape_0
         wave_real, wave_imag, dt = adorym.multislice_propagate_batch(block,
                                                          block_probe_real_batch, block_probe_imag_batch, energy_ev,
                                                          psize_cm, delta_cm=slice_spacing_cm,
                                                          obj_batch_shape=block_delta_batch.shape,
                                                          return_fft_time=True,
-                                                         repeating_slice=n_slices)
+                                                         repeating_slice=n_slices, device=device_obj)
         comm.Barrier()
         dt_fft_prop = dt
+        t_reshape_0 = time.time()
         wave_real = w.to_numpy(wave_real)
         wave_imag = w.to_numpy(wave_imag)
         wavefield = wave_real + 1j * wave_imag
+        dt_reshape = time.time() - t_reshape_0 + dt_reshape
         # -----------------------------------------
         t_write_0 = time.time()
-        comm.Barrier()
-        f_out = h5py.File(os.path.join(path_prefix, 'size_{}'.format(this_size), 'nd_{}'.format(n_nodes),
-                                      'pfft_nslices_{}_output_{}.h5'.format(n_slices, i)),
-                                      'w', driver='mpio', comm=comm)
-        print(wavefield.dtype)
-        dset = f_out.create_dataset('wavefield', original_grid_shape[:-1], dtype='complex64', chunks=True)
-        pos_ind_ls = range(rank, n_blocks, n_ranks)
-        for ind, i_pos in enumerate(pos_ind_ls):
-            line_st = i_pos // n_blocks_x * block_size
-            line_end = min([line_st + block_size, original_grid_shape[0]])
-            px_st = i_pos % n_blocks_x * block_size
-            px_end = min([px_st + block_size, original_grid_shape[1]])
-            dset[line_st:line_end, px_st:px_end] = wavefield[ind,
-                                                              safe_zone_width:safe_zone_width + (line_end - line_st),
-                                                              safe_zone_width:safe_zone_width + (px_end - px_st)]
-        comm.Barrier()
-        f_out.close()
-        comm.Barrier()
+#        comm.Barrier()
+#        f_out = h5py.File(os.path.join(path_prefix, 'size_{}'.format(this_size), 'nd_{}'.format(n_nodes),
+#                                      'pfft_nslices_{}_output_{}.h5'.format(n_slices, i)),
+#                                      'w', driver='mpio', comm=comm)
+#        print(wavefield.dtype)
+#        dset = f_out.create_dataset('wavefield', original_grid_shape[:-1], dtype='complex64', chunks=True)
+#        pos_ind_ls = range(rank, n_blocks, n_ranks)
+#        for ind, i_pos in enumerate(pos_ind_ls):
+#            line_st = i_pos // n_blocks_x * block_size
+#            line_end = min([line_st + block_size, original_grid_shape[0]])
+#            px_st = i_pos % n_blocks_x * block_size
+#            px_end = min([px_st + block_size, original_grid_shape[1]])
+#            dset[line_st:line_end, px_st:px_end] = wavefield[ind,
+#                                                              safe_zone_width:safe_zone_width + (line_end - line_st),
+#                                                              safe_zone_width:safe_zone_width + (px_end - px_st)]
+#        comm.Barrier()
+#        f_out.close()
+#        comm.Barrier()
         dt_write = time.time() - t_write_0
         # -----------------------------------------
         dt_total = time.time() - t_tot_0
@@ -232,8 +237,8 @@ for i in range(n_repeats):
     comm.Barrier()
 
     if rank == 0:
-        print('PFFT: For size {}, average dt = {} s.'.format(this_size, dt_total))
-        f.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(i, n_nodes, this_size, n_slices, safe_zone_width, n_blocks_y, n_blocks_x, block_size, n_ranks, dt_total, dt_read_div, dt_write, dt_fft_prop))
+        print_flush('PFFT: For size {}, average dt = {} s.'.format(this_size, dt_total))
+        f.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(i, n_nodes, this_size, n_slices, safe_zone_width, n_blocks_y, n_blocks_x, block_size, n_ranks, dt_total, dt_read_div, dt_reshape, dt_fft_prop))
         f.flush()
         os.fsync(f.fileno())
 
